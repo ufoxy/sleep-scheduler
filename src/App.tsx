61b28da3d749e3535469045c6f49874eed83ff89
foxyuh <arthurbryansk@gmail.com>
2022-06-29 17:31:04 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,18 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
 function App() {
-
   return (
     <div className="App">
-      <main className='main-container'>
-        <section className='section section-container'>
-            <article className='article'>
-              <p>oi</p>
-            </article>
+      <main className="main-container">
+        <section className="section section-container">
+          <article className="article">
+            <p>oi</p>
+          </article>
         </section>
       </main>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
