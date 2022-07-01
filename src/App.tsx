d675fae0dc88f1397e824a2aad6bbeadf1442bde
foxyuh <arthurbryansk@gmail.com>
2022-07-01 20:04:59 -0300

    implementação do aos no código

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,25 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import "./App.css";
 import SleepOrWakeUp from "./components/sleep-or-wakeup";
 import StaticTimePickerLandscape from "./components/static-time-picker-landscape";
+import AOS from 'aos';
+import "aos/dist/aos.css";
 
 function App() {
+
+  useEffect(() => {
+    AOS.init({
+      offset: 200,
+      duration: 400,
+      easing: 'ease-out-sine',
+      delay: 150,
+    });
+  }, []);
+
   return (
     <div className="App">
       <main>
-        <section className="section section-container">
+        <section className="section section-container" data-aos="fade-up" >
           <SleepOrWakeUp />
           <StaticTimePickerLandscape />
         </section>
