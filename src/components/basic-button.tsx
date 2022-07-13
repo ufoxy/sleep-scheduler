9bcf6cd685036fa8218c78d43d2cf71bdd630cf5
foxyuh <arthurbryansk@gmail.com>
2022-07-13 01:04:37 -0300

    href property added

--- src/components/basic-button.tsx
+++ src/components/basic-button.tsx
@@ -1,9 +1,11 @@
 import Button from "@mui/material/Button";
 
-export default function BasicButton({ children, color }:any) {
+export default function BasicButton({ children, color, href }:any) {
   return (
-    <Button variant="contained" color={color} style={{width: "250px", height: "45px", fontWeight: '600', fontSize: '16px'}}>
-      {children}
-    </Button>
+    <a href={href}>
+      <Button variant="contained" color={color} style={{width: "250px", height: "45px", fontWeight: '600', fontSize: '16px'}}>
+        {children}
+      </Button>
+    </a>
   );
 }
