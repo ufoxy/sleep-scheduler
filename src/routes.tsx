ae0abf16d6ba86b74c0df4c893945acc5d7bdfb1
foxyuh <arthurbryansk@gmail.com>
2022-07-14 05:07:03 -0300

    Update routes.tsx

--- src/routes.tsx
+++ src/routes.tsx
@@ -2,7 +2,7 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import SleepOrWakeUp from "./components/sleep-or-wakeup";
 import SelectTime from "./components/select-time";
 import Schedule from "./components/schedule";
-import About from "./pages/about";
+import About from "./pages/guide";
 import Home from "./pages/home";
 
 export default function AppRouter() {
@@ -10,7 +10,7 @@ export default function AppRouter() {
     <Router>
       <Routes>
         <Route path="/home" element={<Home />} />
-        <Route path="/about" element={<About />} />
+        <Route path="/guide" element={<About />} />
         <Route path="/sleep-or-wake-up" element={<SleepOrWakeUp />} />
         <Route path="/select-time" element={<SelectTime />} />
         <Route path="/schedule" element={<Schedule />} />
