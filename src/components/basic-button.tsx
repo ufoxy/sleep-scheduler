f7196270e411bcf7377da0953c373de0277ed13e
foxyuh <arthurbryansk@gmail.com>
2022-07-14 05:05:38 -0300

    nova propriedade no basic button: variant

--- src/components/basic-button.tsx
+++ src/components/basic-button.tsx
@@ -1,9 +1,9 @@
 import Button from "@mui/material/Button";
 
-export default function BasicButton({ children, color, href }:any) {
+export default function BasicButton({ children, color, variant, href, size = { width: "250px", height: "45px", fontWeight: '600', fontSize: '16px' } }: any) {
   return (
     <a href={href}>
-      <Button variant="contained" color={color} style={{width: "250px", height: "45px", fontWeight: '600', fontSize: '16px'}}>
+      <Button variant={variant} color={color} style={size}>
         {children}
       </Button>
     </a>
