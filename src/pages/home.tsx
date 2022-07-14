ecf0c2e27895e657964310db142e2fe96248565f
foxyuh <arthurbryansk@gmail.com>
2022-07-14 05:06:52 -0300

    adicionado novos elementos na home.tsx

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,14 +1,26 @@
-import * as React from 'react';
 import Navbar from '../components/navbar';
+import sweetDreamSleep from '../assets/imgs/sweet-dream-sleep.png'
 import './home.css'
+import BasicButton from '../components/basic-button';
 
 function Home() {
     return (
-        <div className="container">
+        <div className="super container">
             <Navbar />
             <main>
-                <section>
-                    oi
+                <section className="home-section flex-row">
+                    <img src={sweetDreamSleep} alt="Sleeping Girl" className="home-img" />
+                    <div className="content-home-container flex-collumn">
+                        <div className='content'>
+                            <h2>Calcule o seu sono</h2>
+                            <p>Veja que horas deve acordar ou ir dormir para ter uma boa noite de sono usando a calculadora a seguir:</p>
+                            <div className="button-home-container">
+                                <BasicButton children={"Calculate"} color={"primary"} variant={"contained"} href={"sleep-or-wake-up"} />
+                                <BasicButton children={"Guide"} color={"primary"} variant={"outlined"} href={"home"}
+                                    size={{ width: "180px", height: "45px", fontWeight: '600', fontSize: '16px' }} />
+                            </div>
+                        </div>
+                    </div>
                 </section>
             </main>
         </div>
