e571ae7a8e4d735cfa5d23cb589c37f5a686ecce
foxyuh <arthurbryansk@gmail.com>
2022-07-16 04:33:02 -0300

    desabilitando guide para desenvolvimento

--- src/components/basic-button.tsx
+++ src/components/basic-button.tsx
@@ -1,8 +1,13 @@
 import Button from "@mui/material/Button";
 
-export default function BasicButton({ children, color, variant, href, size = { width: "250px", height: "45px", fontWeight: '600', fontSize: '16px' } }: any) {
+export default function BasicButton({ children, color, variant, href, 
+  size = { width: "250px", height: "45px", fontWeight: '600', fontSize: '16px' }, 
+  onclick=false}: any) {
+
+
+
   return (
-    <a href={href}>
+    <a href={href} onClick={onclick} >
       <Button variant={variant} color={color} style={size}>
         {children}
       </Button>
--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,23 +1,35 @@
 import github from "../assets/imgs/github.svg";
-import './navbar.css';
+import "./navbar.css";
 
 function Navbar() {
     return (
         <header className="header">
-            <div className="flex-row" style={{ justifyContent: 'center', alignItems: 'center' }}>
+            <div
+                className="flex-row"
+                style={{ justifyContent: "center", alignItems: "center" }}
+            >
                 <h1>
-                    <a href="/home">Sleep Schedule</a>
+                    <a href="/home">Sleep Scheduler</a>
                 </h1>
-                <a style={{ paddingLeft: '15px' }} href="https://github.com/ufoxy/sleep-schedule" aria-label="Homepage" title="GitHub">
+                <a
+                    style={{ paddingLeft: "15px" }}
+                    href="https://github.com/ufoxy/sleep-schedule"
+                    aria-label="Homepage"
+                    title="GitHub"
+                >
                     <img src={github} alt="" className="icon-img" />
                 </a>
             </div>
 
             <nav className="stroke" id="mainNav">
                 <ul>
-                    <li><a href="/home">Home</a></li>
+                    <li>
+                        <a href="/home">Home</a>
+                    </li>
                     <p className="centralize">|</p>
-                    <li><a href="/guide">Guide</a></li>
+                    <li>
+                        <a href="#" onClick={() => alert("Page under development")}>Guide</a>
+                    </li>
                 </ul>
             </nav>
         </header>
