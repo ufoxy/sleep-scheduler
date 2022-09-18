c143d10166deb2d3d779309df0cabb7647381b4e
foxyuh <arthurbryansk@gmail.com>
2022-09-18 15:19:46 -0300

    correção de rota

--- src/components/select-time.tsx
+++ src/components/select-time.tsx
@@ -83,7 +83,6 @@ export default function SelectTime() {
                 ? calculateFallAsleep
                 : calculateWakeUp
             }
-            anchor={true}
           />
         </div>
       </section>
