3eed58ed68e03b4f6027344ec563dc645d54eca8
foxyuh <arthurbryansk@gmail.com>
2022-09-18 15:11:35 -0300

    Update select-time.tsx

--- src/components/select-time.tsx
+++ src/components/select-time.tsx
@@ -78,7 +78,6 @@ export default function SelectTime() {
           <ContinueAndBackButton
             children="Continue"
             href={"/schedule"}
-            nav={false}
             onclick={
               wakeUpOrFallAsleep == "Fall-Asleep"
                 ? calculateFallAsleep
