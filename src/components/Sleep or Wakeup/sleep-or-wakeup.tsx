eecd7e92556873731d23a8b0ebff66840b8e9db1
foxyuh <arthurbryansk@gmail.com>
2022-10-29 21:01:27 -0300

    Update sleep-or-wakeup.tsx

--- src/components/Sleep or Wakeup/sleep-or-wakeup.tsx
+++ src/components/Sleep or Wakeup/sleep-or-wakeup.tsx
@@ -1,8 +1,8 @@
 import { useEffect, useState } from "react";
 import useLocalStorageState from "use-local-storage-state";
 import ContinueAndBackButton from "../Continue and Back Button/continue-and-back-button";
-import wakeUp from "../assets/imgs/wake-up.png";
-import fallSleep from "../assets/imgs/zzz.png";
+const wakeUp = require("../assets/imgs/wake-up.png") as string;
+const fallSleep = require("../assets/imgs/zzz.png") as string;
 import "./sleep-or-wakeup.css";
 import "aos/dist/aos.css";
 
